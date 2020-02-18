5f9f52a5f08a057e18e00bbb0c7140758cb79cb1
Wendy Williams <wllwen007@gmail.com>
2020-02-18 11:13:37 +0100

    update split and concat

--- split_and_concat.py
+++ split_and_concat.py
@@ -12,40 +12,58 @@ if __name__ == '__main__':
     parser.add_argument('--datacolumn', help='datacolumn', default='CORRECTED_DATA', type=str)
     parser.add_argument('--skipconcat', help='skipconcat', default=False, action='store_true')
     parser.add_argument('--skipsplit', help='skipsplit', default=False, action='store_true')
+    parser.add_argument('--keepfiles', help='keep intermediate files', default=False, action='store_true')
     parser.add_argument('--noclobber', help='clobber is off', default=False, action='store_true')
+    parser.add_argument('--dryrun', help='only print commands', default=False, action='store_true')
+    parser.add_argument('--subnchan', help='number of channels in each band', default=40, type=int)
+    parser.add_argument('-s','--suffix', help='add a suffix to the obsid', default='', type=str)
+    parser.add_argument('-p','--prefix', help='add a prefix to the obsid', default='', type=str)
     parser.add_argument('outmsroot', help='Output MS root name', type=str)
     parser.add_argument('inmsfile', help='Measurement sets', nargs='+', type=str)
     args = parser.parse_args()
     
     inmsfiles = args.inmsfile
-    outmsroot = args.outmsroot
     datacolumn = args.datacolumn
-    
+    suffix = args.suffix
+    prefix = args.prefix
+
+    outmsroot = prefix + args.outmsroot + suffix
     
     nchan = None
     
     
+
+
     if not args.skipsplit:
 
         allms = []
         for inms in inmsfiles:
-            ### split
-            subnchan=40
+            # get basename of this ms
+	    inmsroot = inms.split('/')[-1].split('.')[0]
+            print('input ms root: ',inmsroot)
+
+            ### split in freq
+            subnchan=args.subnchan
+	    # all should be the same so we do this once
             if nchan is None:
                 t0_spw = pt.table(inms+'/SPECTRAL_WINDOW')
                 nchan = t0_spw.getcol('NUM_CHAN')[0]
                 fchan = t0_spw.getcol('CHAN_FREQ')[0]
                 dchan = t0_spw.getcol('CHAN_WIDTH')[0]
                 
+	    # keep a list of the bands created for this timechunk
             allmsi = []
-            inmsroot = inms.split('.')[0]
             for ii,i in enumerate(range(0,nchan,subnchan)):
-                inmsband = '{inmsroot}_BAND{ii:03d}.MS'.format(ii=ii, inmsroot=inmsroot)
-                cmd= 'DPPP numthreads=32 msin={inms} msin.datacolumn={datacolumn} msout={inmsband} steps=[filter] filter.type=filter  filter.startchan={i:d} filter.nchan={subnchan:d}'.format(i=i,subnchan=subnchan, inms=inms, outmsroot=outmsroot, datacolumn=datacolumn, inmsband=inmsband)
-                print(cmd)
-                os.system(cmd)
-                allmsi.append(inmsband)
+                outmsband = '{prefix}{inmsroot}{suffix}_BAND{ii:03d}.MS'.format(ii=ii, inmsroot=inmsroot, suffix=suffix,prefix=prefix)
+		print('Out MS band',outmsband)
+                cmd= 'DPPP numthreads=32 msin={inms} msin.datacolumn={datacolumn} msout={outmsband} steps=[filter] filter.type=filter  filter.startchan={i:d} filter.nchan={subnchan:d}'.format(i=i,subnchan=subnchan, inms=inms, outmsband=outmsband, datacolumn=datacolumn)
+                print(inms+' -> '+outmsband)
+		if not args.dryrun:
+                    print(cmd)
+                    os.system(cmd)
+                allmsi.append(outmsband)
                 
+	    # add this list to the list of all
             allms.append(allmsi)
         
 
@@ -60,8 +78,9 @@ if __name__ == '__main__':
  
     
     if not args.skipconcat:
-        ### concatenate
+        ### concatenate in time
         
+	# for each of the bands created above
         for bandi in range(allms.shape[1]):
         
             mergems = '{outmsroot}_BAND{ii:03d}.MS'.format(ii=bandi, outmsroot=outmsroot)
@@ -76,38 +95,20 @@ if __name__ == '__main__':
                     sys.exit()
             
             
+            print('concatenating in time')
+            print(mergems+': '+','.join(allms[:,bandi]))
+            if not args.dryrun:
+                t = pt.table(allms[:,bandi])
+                t.sort('TIME,ANTENNA1,ANTENNA2').copy(mergems, deep=True)
             
-            print('concatenating')
-            print(mergems, allms[:,bandi])
-            t = pt.table(allms[:,bandi])
-            t.sort('TIME,ANTENNA1,ANTENNA2').copy(mergems, deep=True)
-            
-            print('concatenating done')
-        
-                
+                print('concatenating done')
 
-            pt.addImagingColumns(mergems)
-        
-        
-        
-        
-    
-    ## Create time-chunks
-    #print('Splitting in time...')
-    #tc = 0
-    
-    #t = pt.table(mergems, ack=False)
-    #starttime = t[0]['TIME']
-    #endtime   = t[t.nrows()-1]['TIME']
-    #hours = (endtime-starttime)/3600.
-    #print(mergems+' has length of '+str(hours)+' h.')
+		print('add imaging columns')
+                pt.addImagingColumns(mergems)
 
-    #for timerange in np.array_split(sorted(set(t.getcol('TIME'))), round(hours)):
-        #print('%02i - Splitting freqrange %f %f' % (tc, timerange[0], timerange[-1]))
-        #t1 = t.query('TIME >= ' + str(timerange[0]) + ' && TIME <= ' + str(timerange[-1]), sortlist='TIME,ANTENNA1,ANTENNA2')
-        #splitms = groupname+'_TC%02i.MS' % tc
-        ##lib_util.check_rm(splitms)
-        #t1.copy(splitms, True)
-        #t1.close()
-        #tc += 1
-    #t.close()
+                # remove the intermediate files unless asked to keep them
+	        if not args.keepfiles:
+	            for ms in allms[:,bandi]:
+		        print('removing '+ms)
+		        os.system('rm -rf '+ ms)
+        
